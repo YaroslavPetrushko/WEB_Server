8c11e9be73df766c897d1233189dfc31d7994563
Yaroslav Petrushko <yaroslavpetrushko.off@gmail.com>
2026-02-12 18:14:14 +0200

    Lab1
    Task 2

--- lab1-server/server.js
+++ lab1-server/server.js
@@ -3,7 +3,20 @@ const port = 4000;
 const server = http.createServer((req,res)=>{
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('Hello Server!');
+    
+    METHOD=req.method;
+    URL=req.url;
+    DATE = new Date().toISOString();
+
+    if(URL==='/'){
+        res.end('Home page');
+    }
+    else if(URL==='/about'){
+        res.end('About page');
+    }
+    else res.end('Page not found');
+    
+    console.log(`You made a ${METHOD} request to ${URL}. Timestamp: ${DATE}`);
 });
 
 server.listen(port, () => { console.log(`Server is running on port ${port}`); });
