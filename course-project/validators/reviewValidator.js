fbaa34164e06d2943da997a9a467c67cb271dcad
Yaroslav Petrushko <yaroslavpetrushko.off@gmail.com>
2026-05-18 22:34:01 +0300

    Add drawer to selected course on main page. Show Review in course details and profile

--- course-project/validators/reviewValidator.js
+++ course-project/validators/reviewValidator.js
@@ -1,14 +1,13 @@
 const Joi = require('joi');
-const { create } = require('../models/User');
 
 const createReviewSchema = Joi.object({
   rating: Joi.number()
     .min(1)
-    .max(10)
+    .max(5)
     .required()
     .messages({
       'number.min': 'Оцінка не може бути менше 1',
-      'number.max': 'Оцінка не може бути більше 10',
+      'number.max': 'Оцінка не може бути більше 5',
       'any.required': 'Оцінка обов\'язкова'
     }),
 
